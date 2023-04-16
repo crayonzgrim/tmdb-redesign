bd1f3efc49f21face8811beae005458d75adc8f4
crayonzgrim <crayonzgrim@gmail.com>
2023-04-16 15:17:02 +0900

    :hammer: About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic iure
+        ratione, dolorem omnis, ab libero consequuntur officiis odit tempore
+        corrupti alias aut provident deleniti in quo. Quo molestias totam
+        cupiditate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic iure
+        ratione, dolorem omnis, ab libero consequuntur officiis odit tempore
+        corrupti alias aut provident deleniti in quo. Quo molestias totam
+        cupiditate. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+      <p>
+        Hic iure ratione, dolorem omnis, ab libero consequuntur officiis odit
+        tempore corrupti alias aut provident deleniti in quo. Quo molestias
+        totam cupiditate.
+      </p>
+    </div>
+  );
+}
